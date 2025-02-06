e029a13436e6a772e5430ad27c774d2949ef3f73
Hseyaj <122300889+hseyaj04@users.noreply.github.com>
2025-02-06 18:19:39 +0000

    fixed a bug

--- async-bg-color-changer/script.js
+++ async-bg-color-changer/script.js
@@ -4,14 +4,17 @@ const stop = document.getElementById('stop')
 
 let clear
 start.addEventListener('click', (e)=>{
-    clear = setInterval(()=>{
-        // document.body.style.backgroundColor = `#${Math.floor(Math.random()*(16**6)).toString(16).padStart(6, '0')}`
-
-        document.body.style.backgroundColor = `#${Math.floor((Math.random()*10)%9)}${Math.floor(Math.random()*(10**5))}`
-    }, 1000)
+    if(!clear){ //When you don't apply this the on pressing start more then once more instances strat and changes color
+        clear = setInterval(()=>{
+            // document.body.style.backgroundColor = `#${Math.floor(Math.random()*(16**6)).toString(16).padStart(6, '0')}`
+    
+            document.body.style.backgroundColor = `#${Math.floor((Math.random()*10)%1)}${Math.floor(Math.random()*(10**5))}`
+        }, 1000)
+    } 
 })
 
 stop.addEventListener('click',(e)=>{
     clearInterval(clear)
+    clear = null
 }
 )
